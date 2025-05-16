136bb8a134e9790cbcd5e26d85d93f1ae8407289
rigonnhasani <rigonhasani33@gmail.com>
2025-05-16 19:37:57 +0200

    Update index.js (#2)

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
-// api/index.js
+
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
-
+app.get('/', (req, res) => {
+  res.send('Mini-SaaS API is running!');
+});
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'ok' });
 });
